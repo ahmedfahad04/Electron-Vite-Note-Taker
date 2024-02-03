4a301e8d414b9802fe50da89917acb9156069bdf
ahmedfahad04 <bsse1204@iit.du.ac.bd>
2024-02-03 17:50:06 +0600

    sidebar ui design done

--- src/renderer/src/components/NotePreviewList.tsx
+++ src/renderer/src/components/NotePreviewList.tsx
@@ -1,10 +1,19 @@
 import { mockNotes } from '@/store/mocks'
 import { ComponentProps } from 'react'
+import { twMerge } from 'tailwind-merge'
 import { NotePreview } from './NotePreview'
 
-export const NotePreviewList = ({ ...props }: ComponentProps<'ul'>) => {
+export const NotePreviewList = ({ className, ...props }: ComponentProps<'ul'>) => {
+  if (mockNotes.length === 0) {
+    return (
+      <ul className={twMerge('text-center pt-4', className)} {...props}>
+        <span>No Notes yet!</span>
+      </ul>
+    )
+  }
+
   return (
-    <ul {...props}>
+    <ul className={className} {...props}>
       {mockNotes.map((note) => (
         <NotePreview key={note.title + note.lastEditTime} {...note} />
       ))}
