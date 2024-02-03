8775bf37ef84674b5859213adca849bb9893b6a1
ahmedfahad04 <bsse1204@iit.du.ac.bd>
2024-02-03 15:15:31 +0600

    styling settings done

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,7 +1,7 @@
 function App(): JSX.Element {
   return (
-    <div>
-      <h2>Hello world</h2>
+    <div className="flex h-full items-center justify-center">
+      <h2 className="bg-red-500 text-4xl">Hello from Electron</h2>
     </div>
   )
 }
