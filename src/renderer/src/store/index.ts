1f22451233e499d0343ce733d98c3074e77659f2
ahmedfahad04 <bsse1204@iit.du.ac.bd>
2024-02-03 23:00:31 +0600

    electron: invoke getNotes from frontend

--- src/renderer/src/store/index.ts
+++ src/renderer/src/store/index.ts
@@ -1,14 +1,26 @@
-import { mockNotes } from '@/store/mocks'
 import { NoteInfo } from '@shared/model'
 import { atom } from 'jotai'
+import { unwrap } from 'jotai/utils'
 
-export const notesAtom = atom<NoteInfo[]>(mockNotes)
+// get notes from local file system
+const loadNotes = async () => {
+  const notes = await window.context.getNotes()
+
+  // sort by most recently edited version
+  return notes.sort((a, b) => b.lastEditTime - a.lastEditTime)
+}
+
+const notesAtomAsync = atom<NoteInfo[] | Promise<NoteInfo[]>>(loadNotes())
+
+// used unwrap as notesAtomAsync is a async func while atom is non-async
+export const notesAtom = unwrap(notesAtomAsync, (prev) => prev)
+
 export const selectedNoteIndexAtom = atom<number | null>(null)
 export const selectedNoteAtom = atom((get) => {
   const notes = get(notesAtom)
   const selectedNoteIndex = get(selectedNoteIndexAtom)
 
-  if (selectedNoteIndex == null) return null
+  if (selectedNoteIndex == null || !notes) return null
   const selectedNote = notes[selectedNoteIndex]
 
   return {
@@ -19,6 +31,9 @@ export const selectedNoteAtom = atom((get) => {
 
 export const createEmptyNoteAtom = atom(null, (get, set) => {
   const notes = get(notesAtom)
+
+  //! as notes warns about 'undefined', we added this condition !notes -> return
+  if (!notes) return
 
   const title = `Note ${notes.length + 1}`
   const newNote: NoteInfo = {
@@ -35,7 +50,7 @@ export const deleteNoteAtom = atom(null, (get, set) => {
   const notes = get(notesAtom)
   const selectedNote = get(selectedNoteAtom)
 
-  if (!selectedNote) return null
+  if (!selectedNote || !notes) return
 
   set(
     notesAtom,
