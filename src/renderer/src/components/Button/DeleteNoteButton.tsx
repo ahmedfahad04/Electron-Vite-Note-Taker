d0a12c085061f06603fa0846ecbf741531cda7a4
ahmedfahad04 <bsse1204@iit.du.ac.bd>
2024-02-03 21:42:30 +0600

    added create and del note atom

--- src/renderer/src/components/Button/DeleteNoteButton.tsx
+++ src/renderer/src/components/Button/DeleteNoteButton.tsx
@@ -1,9 +1,13 @@
 import { ActionButton, ActionButtonProps } from '@/components'
+import { deleteNoteAtom } from '@/store'
+import { useSetAtom } from 'jotai'
 import { FaRegTrashAlt } from 'react-icons/fa'
 
 export const DeleteNoteButton = ({ ...props }: ActionButtonProps) => {
+  const deleteNote = useSetAtom(deleteNoteAtom)
+
   return (
-    <ActionButton {...props}>
+    <ActionButton onClick={() => deleteNote()} {...props}>
       <FaRegTrashAlt className="w-4 h-4 text-zinc-400" />
     </ActionButton>
   )
--- src/renderer/src/store/index.ts
+++ src/renderer/src/store/index.ts
@@ -3,7 +3,7 @@ import { NoteInfo } from '@shared/model'
 import { atom } from 'jotai'
 
 export const notesAtom = atom<NoteInfo[]>(mockNotes)
-export const selectedNoteIndexAtom = atom<number>(0)
+export const selectedNoteIndexAtom = atom<number | null>(null)
 export const selectedNoteAtom = atom((get) => {
   const notes = get(notesAtom)
   const selectedNoteIndex = get(selectedNoteIndexAtom)
@@ -15,4 +15,32 @@ export const selectedNoteAtom = atom((get) => {
     ...selectedNote
     // content: `Hello from Note${selectedNoteIndex}`
   }
+})
+
+export const createEmptyNoteAtom = atom(null, (get, set) => {
+  const notes = get(notesAtom)
+
+  const title = `Note ${notes.length + 1}`
+  const newNote: NoteInfo = {
+    title: title,
+    content: 'Sample Content 555',
+    lastEditTime: new Date().getTime()
+  }
+
+  set(notesAtom, [newNote, ...notes.filter((note) => note.title !== newNote.title)])
+  set(selectedNoteIndexAtom, 0)
+})
+
+export const deleteNoteAtom = atom(null, (get, set) => {
+  const notes = get(notesAtom)
+  const selectedNote = get(selectedNoteAtom)
+
+  if (!selectedNote) return null
+
+  set(
+    notesAtom,
+    notes.filter((note) => note.title !== selectedNote.title)
+  )
+
+  set(selectedNoteIndexAtom, null)
 })
